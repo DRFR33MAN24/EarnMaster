41d1e7828241aba7d8dd4163e5e7c0e1e6e839ae
Gordon Freeman <drfr33man@protonmail.com>
2022-06-14 17:30:57 +0300

    working on home screen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -5,23 +5,36 @@ import {
   Divider,
   TopNavigation,
 } from '@ui-kitten/components';
-import React, { useState } from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 
-
-export const HomeScreen = ({ navigation }) => {
-
-
+export const HomeScreen = ({navigation}) => {
   // const navigateDetails = () => {
   //   navigation.navigate('Details');
   // };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <Divider />
 
-      <ScrollView style={{ flex: 1 }}>
-
+      <ScrollView style={{flex: 1}}>
+        <View
+          style={{
+            padding: 20,
+            margin: 20,
+            flex: 1,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignContent: 'center',
+          }}>
+          <Button>Watch Ads</Button>
+          <Button>Complete offers</Button>
+          <Button>Gamble</Button>
+          <Button>Gamble</Button>
+          <Button>Gamble</Button>
+          <Button>Gamble</Button>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
