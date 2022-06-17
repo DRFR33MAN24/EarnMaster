59fd0673c0b393a3800daa3c9f0a6098b61c833b
Gordon Freeman <drfr33man@protonmail.com>
2022-06-17 17:37:24 +0300

    some refactoring

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+import {default as theme} from './src/theme.json';
 import {AppNavigator} from './src/Screens/Navigator';
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
+  <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
     <AppNavigator />
   </ApplicationProvider>
 );
