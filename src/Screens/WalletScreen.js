78a01e1ac3ea23649616c101c84a30d9f88bfa26
Gordon Freeman <drfr33man@protonmail.com>
2022-06-18 21:20:21 +0300

    working on wallet screen

--- src/Screens/WalletScreen.js
+++ src/Screens/WalletScreen.js
@@ -1,12 +1,17 @@
-import { Text, Divider } from '@ui-kitten/components';
-import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCoins, faWallet } from '@fortawesome/free-solid-svg-icons';
+import {Text, Divider, Card, Button} from '@ui-kitten/components';
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, View, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCoins, faWallet} from '@fortawesome/free-solid-svg-icons';
 
 export const walletIcon = props => (
-  <FontAwesomeIcon {...props} icon={faWallet} size={25} style={{ color: props.style.tintColor }} />
+  <FontAwesomeIcon
+    {...props}
+    icon={faWallet}
+    size={25}
+    style={{color: props.style.tintColor}}
+  />
 );
 export const WalletScreenTopBar = () => {
   let navigation = useNavigation();
@@ -23,22 +28,54 @@ export const WalletScreenTopBar = () => {
         }}>
         {/* <Text style={styles.badge}> 2 </Text> */}
 
-        <FontAwesomeIcon icon={faCoins} size={25} style={{ color: 'gold' }} />
+        <FontAwesomeIcon icon={faCoins} size={25} style={{color: 'gold'}} />
         <Text> 100</Text>
       </View>
     </View>
   );
 };
-export const WalletScreen = ({ navigation }) => {
+export const WalletScreen = ({navigation}) => {
   // const navigateDetails = () => {
   //   navigation.navigate('Details');
   // };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <Divider />
-
-      <ScrollView style={{ flex: 1 }}></ScrollView>
+      <View style={{margin: 10}}>
+        <Card>
+          <Text>your total earnings:</Text>
+          <Text style={styles.bigBoldText}>$71.5</Text>
+          <View style={{marginVertical: 8}}>
+            <Button>Withdraw</Button>
+          </View>
+        </Card>
+      </View>
+      <View style={{margin: 10}}>
+        <Card>
+          <Text>Transaction history:</Text>
+        </Card>
+      </View>
+      <ScrollView style={{flex: 1, margin: 10}}>
+        <Card>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{color: 'red'}}>-$5.00</Text>
+            <Text> Visa prepaid card.</Text>
+          </View>
+          <Text style={styles.dateText}>12/12/12</Text>
+        </Card>
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  bigBoldText: {
+    fontSize: 40,
+    fontFamily: 'normal',
+  },
+  dateText: {
+    fontSize: 10,
+    fontFamily: 'normal',
+  },
+});
