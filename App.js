f6417c8886bef5b9d75d52917d31028397558691
Gordon Freeman <drfr33man@protonmail.com>
2022-06-19 03:35:56 +0300

    working on wallet screen

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
 import {default as theme} from './src/theme.json';
+import {default as mapping} from './mapping.json';
 import {AppNavigator} from './src/Screens/Navigator';
 
 export default () => (
-  <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+  <ApplicationProvider
+    {...eva}
+    theme={{...eva.light, ...theme}}
+    customMapping={mapping}>
     <AppNavigator />
   </ApplicationProvider>
 );
