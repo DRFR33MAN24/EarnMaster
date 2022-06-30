2b4df448c7b5e8e3156d9f579797ccffb4fef762
Gordon Freeman <drfr33man@protonmail.com>
2022-06-30 07:22:42 +0300

    reworked surveys component

--- src/Components/Survey.js
+++ src/Components/Survey.js
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import {Divider, Card, Text} from '@ui-kitten/components';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faClock, faStar} from '@fortawesome/free-solid-svg-icons';
+import {faClock, faS, faStar} from '@fortawesome/free-solid-svg-icons';
 import {ThemeContext} from '../../theme-context';
 import {glass} from '../Constants/images';
 export const Survey = ({data}) => {
@@ -18,37 +18,128 @@ export const Survey = ({data}) => {
     <View
       style={{
         width: '50%',
-        // minHeight: 200,
+        height: 120,
         maxWidth: 200,
-      }}>
-      <Card>
-        {/* <View style={{ position: 'absolute', backgroundColor: 'pink', width: '150%', height: '150%', zIndex: -200 }}>
 
-                </View> */}
+        // backgroundColor: 'pink',
+
+        margin: 0,
+        padding: 0,
+      }}>
+      <View
+        style={{
+          // backgroundColor: 'yellow',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 2,
+          padding: 5,
+
+          height: 110,
+          width: '99%',
+        }}>
+        <View
+          style={{
+            position: 'absolute',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '99%',
+            height: 100,
+            borderRadius: 15,
+            // backgroundColor: 'red',
+            zIndex: -200,
+          }}>
+          <Image
+            source={data.imageUri}
+            style={{resizeMode: 'center', width: 64, height: 64}}
+          />
+        </View>
         <View style={styles.glassyBackground}>
-          <Image source={glass} style={{resizeMode: 'stretch'}} />
-        </View>
-        <View style={{marginTop: 1, marginRight: 2, alignItems: 'flex-end'}}>
-          <FontAwesomeIcon icon={faStar} size={25} style={styles.starIcon} />
-        </View>
-        <View style={{marginVertical: 8}}>
-          <Text style={{flex: 1, flexWrap: 'wrap'}}>{data.name}</Text>
+          <Image
+            source={glass}
+            style={{
+              resizeMode: 'cover',
+              height: '100%',
+              width: '100%',
+              borderRadius: 15,
+            }}
+          />
         </View>
         <View
           style={{
-            marginBottom: 1,
-            marginLeft: 2,
-            flexDirection: 'row',
-            // height: '100%',
+            position: 'absolute',
+            top: 4,
+            right: 4,
+            zIndex: 300,
           }}>
-          <FontAwesomeIcon
-            icon={faClock}
-            size={25}
-            style={{color: theme === 'light' ? 'black' : 'white'}}
-          />
-          <Text> {data.timeToComplete}</Text>
+          <FontAwesomeIcon icon={faStar} size={20} style={styles.starIcon} />
         </View>
-      </Card>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 4,
+            left: 4,
+            zIndex: 300,
+            flexDirection: 'row',
+          }}>
+          <FontAwesomeIcon icon={faClock} size={20} />
+          <Text>{data.timeToComplete}</Text>
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            // justifyContent: 'flex-start',
+            // alignItems: 'flex-start',
+            width: '95%',
+            height: 100,
+            // backgroundColor: 'blue',
+          }}>
+          <View
+            style={{
+              // flexDirection: 'row',
+              flex: 1,
+              justifyContent: 'flex-start',
+              // backgroundColor: 'red',
+              padding: 5,
+            }}>
+            <Text>{data.name}</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginBottom: 2,
+            }}>
+            {/* <View
+              style={{
+                // flexDirection: 'row',
+                // flex: 1,
+                // justifyContent: 'flex-end',
+                // backgroundColor: 'red',
+                padding: 5,
+              }}>
+              <Text>{data.details}</Text>
+            </View> */}
+            {/* <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                // flex: 1,
+                // justifyContent: 'flex-end',
+                backgroundColor: 'gold',
+                borderRadius: 10,
+                padding: 5,
+              }}>
+              <FontAwesomeIcon
+                icon={faClock}
+                size={10}
+                // style={{color: props.style.tintColor}}
+              />
+              <Text style={{fontSize: 12}}> 500</Text>
+            </View> */}
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -69,9 +160,12 @@ const styles = StyleSheet.create({
   },
   glassyBackground: {
     position: 'absolute',
-    backgroundColor: '#0E317A',
-    width: '150%',
-    height: '150%',
+
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#0E317A',
+    width: '99%',
+    height: 100,
     zIndex: -100,
     opacity: 0.5,
   },
